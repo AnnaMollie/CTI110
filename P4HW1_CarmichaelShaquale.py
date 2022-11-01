469f6ece5debb9c831df2ee19f588908ef7e4230
AnnaMollie <carmichs9860@student.faytechcc.edu>
2022-11-01 14:08:53 -0400

    Update P4HW1_CarmichaelShaquale.py

--- P4HW1_CarmichaelShaquale.py
+++ P4HW1_CarmichaelShaquale.py
@@ -4,4 +4,22 @@
 # 11/01/2022
   
 
-grades = [0,0,0,0,0,0] 
+grades = [] 
+
+
+for grade in range(6):
+    grade = int(input("Enter grade: "))
+    grades.append(grade)
+
+
+print("The grades are:", grades)
+print("The highest grade is: ", max(grades))
+print("The lowest grade is: ", min(grades))
+
+total = sum(grades)
+count = len(grades)
+average = total / count
+print("The total is: ", total)
+print("The average is: ", average)
+
+print
